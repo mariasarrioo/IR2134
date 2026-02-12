f675be26a9a2a5847d9f64b81511f9a626feb9b0
María Sarrió <al415538@uji.es>
2026-02-12 18:30:24 +0100

    test1 rehecho

---
+++ rmf_ws/build/project_config/colcon_command_prefix_build.sh
@@ -0,0 +1,4 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/maria/rmf_ws/install/project_assets/share/project_assets/package.sh"
+. "/home/maria/rmf_ws/install/project_fleet_adapter/share/project_fleet_adapter/package.sh"
+. "/home/maria/rmf_ws/install/project_maps/share/project_maps/package.sh"
