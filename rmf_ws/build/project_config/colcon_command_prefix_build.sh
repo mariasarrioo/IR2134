b8f77412036cc32872f5a7af02beb3c98cdcfb43
María Sarrió <al415538@uji.es>
2026-02-12 17:20:32 +0100

    Se abre gazebo

--- rmf_ws/build/project_config/colcon_command_prefix_build.sh
+++
@@ -1,4 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/maria/rmf_ws/install/project_assets/share/project_assets/package.sh"
-. "/home/maria/rmf_ws/install/project_fleet_adapter/share/project_fleet_adapter/package.sh"
-. "/home/maria/rmf_ws/install/project_maps/share/project_maps/package.sh"
